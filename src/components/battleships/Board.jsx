b36b86689d46b64a0a7b5151be6581c4ead2df16
Shannqa <shannqa@gmail.com>
2024-04-11 23:38:14 +0200

    Work on ai attacks

--- src/components/battleships/Board.jsx
+++ src/components/battleships/Board.jsx
@@ -3,39 +3,39 @@ import { BattleshipsContext } from "./Battleships.jsx"
 import styles from "./Battleships.module.css";
 
 function Board({ grid, owner }) {
-  const { playerGrid, computerGrid, setComputerGrid, currentMove, setCurrentMove, computerShipList, setComputerShipList } = useContext(BattleshipsContext);
+  const { playerGrid, setPlayerGrid, computerGrid, setComputerGrid, currentMove, setCurrentMove, computerShipList, setComputerShipList, playerShipList, setPlayerShipList } = useContext(BattleshipsContext);
   const [attackQueue, setAttackQueue] = useState([]);
+  const [attackHits, setAttackHits] = useState([]);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const maxScore = 14;
   
  function playerAttack(coords) {
    if (currentMove === "player") {
     const [row, column] = coords; 
     console.log(coords)
     const attackedCell = computerGrid[row][column];
-    let newState;
+    let newCellValue;
     if (attackedCell === "miss" || attackedCell === "hit") {
       console.log("invalid move");
       return; 
     } else if (attackedCell === null) {
-      newState = "miss";
+      newCellValue = "miss";
     } else {
-      newState = "hit";
-      // on hit, mark the coordinate on the computer ship list
-      setComputerShipList({...computerShipList,
-      [attackedCell]: [
-        ...computerShipList[attackedCell],
-        [row][column] = "hit"
-      ]});
+      newCellValue = "hit";
+      setPlayerScore(playerScore + 1);
+      if (playerScore >= maxScore - 1) {
+        console.log("player won");
+      }
     }
     // update the state of the computer's grid
     setComputerGrid(computerGrid.map((gridRow, rowIndex) => gridRow.map((gridColumn, colIndex) => {
       if (rowIndex == row && colIndex == column) {
-        gridColumn = newState;
+        gridColumn = newCellValue;
         console.log(gridColumn);
       }
       return gridColumn;
     })));
-
-    console.log(computerGrid);
 
     setCurrentMove("computer");
     computerAttack();
@@ -43,23 +43,67 @@ function Board({ grid, owner }) {
      return;
    }
  }
+    
+  function computerAttack() {
+    let coords;
+    let newCellValue;
+    if (attackQueue.length === 0) {
+      coords = getRandomCoords();
+    } else {
+      coords = newQueue.shift();
+    }
+    const [row, column] = coords;
+    const attackedCell = playerGrid[row][column];
+    
+    if (attackedCell === "hit" || attackedCell === "miss") {
+      // invalid move, try again with different random numbers
+      return computerAttack();
+    } else if (attackedCell === null) {
+      newCellValue = "miss";
+    } else {
+      newCellValue = "hit";
+      setComputerScore(computerScore + 1);
+      if (computerScore >= maxScore - 1) {
+        console.log("computer won");
+      } else {
+        setAttackHits(...attackHits, [row, column]);
+        if (attackHits.length == 0) {
+          // if it's the first hit in a row, add four sides of the current hit to the queue
+          setAttackQueue([[row - 1, column], [row + 1, column], [row, column - 1], [row, column + 1]]);
+        } else {
+          // if not, check if the previous hit was adjacent and in which direction
+          console.log(attackHits);
+          console.log(attackHits[attackHits.length - 1]);
+          const [prevRow, prevCol] = attackHits[attackHits.length - 1];
+          if (prevRow == row) {
+            // row is the same
+            let newQueue = [...attackQueue, [row, column - 1], [row, column + 1]]
+            setAttackQueue(newQueue.filter(item => item[0] == row));
+          } else if (prevCol == column) {
+            // column is the same
+            let newQueue = [...attackQueue, [row - 1, column], [row + 1, column]]
+            setAttackQueue(newQueue.filter(item => item[1] == column));
+          }
+        }
+        
+      }
+
+    }
+    // update the state of the player's grid
+    setPlayerGrid(playerGrid.map((gridRow, rowIndex) => gridRow.map((gridColumn, colIndex) => {
+      if (rowIndex == row && colIndex == column) {
+        gridColumn = newCellValue;
+      }
+      return gridColumn;
+    })));
+    
+    setCurrentMove("player");
+  }
   
-  
- function computerAttack() {
-   if (attackQueue.length === 0) {
-     doRandomAttack()
-   }
-   setCurrentMove("player");
- }
-  
-  function doRandomAttack() {
+  function getRandomCoords() {
     const row = Math.floor(Math.random() * 9);
     const column = Math.floor(Math.random() * 9);
-    console.log("random");
-    // const attackedCell = 
-    // if (playerGrid[row][column] === "hit" || playerGrid[row][column] === "miss") {
-    //   return doRandomAttack();
-    // } else if ()
+    return [row, column]
   }
   
   
